1dcb3e304c02f5c2917405b7a238681259e22dcf
Stranix <Phantom2525@gmail.com>
2023-06-01 10:50:07 +0300

    move try except

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -57,59 +57,59 @@ def create_arg_parser():
 
 
 def main():
-    try:
-        parser = create_arg_parser()
-        args = parser.parse_args()
-        print(args)
-        category_id = args.category_id
-        category_start_page = args.start_page
-        category_end_page = args.end_page
-        dest_folder = args.dest_folder
+    parser = create_arg_parser()
+    args = parser.parse_args()
 
-        skip_imgs = args.skip_imgs
-        skip_txt = args.skip_txt
-        json_path = args.json_path
+    category_id = args.category_id
+    category_start_page = args.start_page
+    category_end_page = args.end_page
+    dest_folder = args.dest_folder
+    skip_imgs = args.skip_imgs
+    skip_txt = args.skip_txt
+    json_path = args.json_path
 
-        category_end_page_on_site = get_category_end_page(category_id)
-        if category_end_page > category_end_page_on_site:
-            print('Страниц у выбранной категории:', category_end_page_on_site)
-            print('Поменяйте диапазон для скачивания')
-            raise KeyboardInterrupt
+    category_end_page_on_site = get_category_end_page(category_id)
+    if category_end_page > category_end_page_on_site:
+        print('Страниц у выбранной категории:', category_end_page_on_site)
+        print('Поменяйте диапазон для скачивания')
+        raise KeyboardInterrupt
 
-        if category_start_page > category_end_page:
-            category_end_page = category_end_page_on_site
+    if category_start_page > category_end_page:
+        category_end_page = category_end_page_on_site
 
-        books_id = get_books_id_in_range_pages_in_category(
-            category_id,
-            category_start_page,
-            category_end_page + 1
+    books_id = get_books_id_in_range_pages_in_category(
+        category_id,
+        category_start_page,
+        category_end_page + 1
+    )
+
+    if not books_id:
+        print('Не нашел книг для скачивания. Проверьте диапазон страниц')
+        raise KeyboardInterrupt
+
+    downloaded_books_info = []
+    for book_id in books_id:
+        book = fetch_book(book_id, dest_folder, skip_imgs, skip_txt)
+
+        if not book:
+            continue
+
+        book.download_link = f'{book.download_link}?id={book.id}'
+        downloaded_books_info.append(book.__dict__)
+
+    if downloaded_books_info:
+        save_books_info_as_json_file(
+            'downloaded_books_info.json',
+            downloaded_books_info,
+            json_path,
         )
 
-        if not books_id:
-            print('Не нашел книг для скачивания. Проверьте диапазон страниц')
-            raise KeyboardInterrupt
 
-        downloaded_books_info = []
-        for book_id in books_id:
-            book = fetch_book(book_id, dest_folder, skip_imgs, skip_txt)
-
-            if not book:
-                continue
-
-            book.download_link = f'{book.download_link}?id={book.id}'
-            downloaded_books_info.append(book.__dict__)
-
-        if downloaded_books_info:
-            save_books_info_as_json_file(
-                'downloaded_books_info.json',
-                downloaded_books_info,
-                json_path,
-            )
-
-    except KeyboardInterrupt:
-        print('Работа скрипта остановлена')
-        sys.exit()
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Работа скрипта остановлена')
+        sys.exit()
